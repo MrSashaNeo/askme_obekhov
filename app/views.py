744d5d069829905f9fd99d0ba1109fb73563e1cf
alex <mrsashaneo@gmail.com>
2024-11-13 13:11:07 +0300

    COMMIT 1

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from tempfile import template
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 QUESTIONS = [
@@ -14,24 +14,82 @@ QUESTIONS = [
       } for i in range(30)
 ]
 
+ANSWERS = [
+      {
+            'id':i,
+            'text': f'This is text for answer #{i}'
+      } for i in range(4)
+]
 
+def paginate(object_list,request,per_page):
+      paginator = Paginator(object_list,per_page)
+      page_number = int(request.GET.get('page',1))
+
+      try:
+            page = paginator.page(page_number)
+      except PageNotAnInteger:
+            page = paginator.page(1)
+      except EmptyPage:
+            page = paginator.page(paginator.num_pages)
+      return page
 
 def index(request):
+
+      page=paginate(QUESTIONS,request,10)
+
       return render(
             request, 'index.html',
-            context={'questions': QUESTIONS}
+            context={'questions': page.object_list, 'page_obj': page}
       )
 
 def hot(request):
       hot_questions = copy.deepcopy(QUESTIONS)
       hot_questions.reverse()
+
+      page = paginate(hot_questions, request, 10)
+
       return render(
             request, 'hot.html',
-            context={'questions': hot_questions}
+            context={'questions': page.object_list, 'page_obj': page}
       )
+
+def tagblablabla(request):
+      tag_questions = copy.deepcopy(QUESTIONS)
+      tag_questions.reverse()
+
+      page = paginate(tag_questions, request, 10)
+
+      return render(
+            request, 'tagblablabla.html',
+            context={'questions': page.object_list, 'page_obj': page}
+      )
+
 def question(request, question_id):
       one_question = QUESTIONS[question_id]
+      answers = copy.deepcopy(ANSWERS)
       return render(
             request, 'question.html',
-            context={'item': one_question}
+            context={'item': one_question, 'answers': answers}
+      )
+
+
+def ask(request):
+      return render(
+            request, 'ask.html'
+      )
+
+def login(request):
+      return render(
+            request, 'login.html'
+      )
+
+
+def settings(request):
+      return render(
+            request, 'settings.html'
+      )
+
+def signup(request):
+      return render(
+            request, 'signup.html'
       )
--- askme_obekhov/urls.py
+++ askme_obekhov/urls.py
@@ -22,5 +22,12 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('hot/',views.hot, name='hot'),
     path('question/<int:question_id>',views.question, name='one_question'),
+    path('ask/',views.ask, name='ask'),
+    path('login/', views.login, name='login'),
+    path('settings/', views.settings, name='settings'),
+    path('signup/', views.signup, name='signup'),
+
+    path('tagblablabla/', views.tagblablabla, name='tagblablabla'),
+
     path('admin/', admin.site.urls),
 ]
